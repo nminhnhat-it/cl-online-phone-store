314879add89736d9dfe189530671398bc4893958
Nhật Nguyễn Minh <nhatb2014865@student.ctu.edu.vn>
2023-11-04 13:08:07 +0700

    init vue project

---
+++ frontend/src/router/index.js
@@ -0,0 +1,16 @@
+import { createWebHistory, createRouter } from "vue-router";
+
+const routes = [
+  {
+    path: "/:pathMatch(.*)*",
+    name: "notfound",
+    component: () => import("@/views/NotFound.vue"),
+  },
+]
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
+});
+
+export default router;
---
+++ frontend/src/views/NotFound.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="text-center">
+    <h1 class="text-danger">404</h1>
+    <h1>Sorry, page not found</h1>
+    <div class="d-flex justify-content-center">
+      <p class="pe-1">Return to</p>
+      <router-link class="link-primary link-offset-2 link-underline-opacity-0 link-underline-opacity-100-hover" to="/">Home Page</router-link>
+    </div>
+  </div>
+</template>
